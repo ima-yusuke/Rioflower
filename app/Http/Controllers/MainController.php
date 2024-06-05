98830032426e2fc651099464084687953c3da657
nozaki59 <107387074+nozaki59@users.noreply.github.com>
2024-06-05 15:59:36 +0900

    fix confirm y-scroll

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -17,9 +17,9 @@ class MainController extends Controller
         ["id" => 1, "question" => "どんな雰囲気？", "answer" => ["シック", "モダン", "暗め", "明るめ", "エレガント","シンプル","華やか","その他"], "order" => 1],
         ["id" => 2, "question" => "誰に渡す？", "answer" => ["自分", "友人", "家族", "恋人", "その他"], "order" => 2],
         ["id" => 3, "question" => "いつ渡す？", "answer" => ["誕生日", "クリスマス", "卒業式", "その他"], "order" => 3],
-        ["id" => 4, "question" => "いつ渡す？", "answer" => ["誕生日", "クリスマス", "卒業式", "その他"], "order" => 4],
-        ["id" => 5, "question" => "いつ渡す？", "answer" => ["誕生日", "クリスマス", "卒業式", "その他"], "order" => 5],
-        ["id" => 6, "question" => "いつ渡す？", "answer" => ["誕生日", "クリスマス", "卒業式", "その他"], "order" => 6],
+        ["id" => 4, "question" => "なぜ渡す？", "answer" => ["感謝", "お祝い", "謝罪", "励まし", "その他"], "order" => 4],
+        ["id" => 5, "question" => "どこで渡す？", "answer" => ["家", "レストラン", "公園", "学校", "その他"], "order" => 5],
+        ["id" => 6, "question" => "渡すときの時間帯は？", "answer" => ["朝", "昼", "夕方", "夜", "その他"], "order" => 6]
     ];
 
     public function ShowQuestionPage()
