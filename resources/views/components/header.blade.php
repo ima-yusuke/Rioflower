a698567760616f1f0420d826b9a369b8b6bca2d7
nozaki59 <107387074+nozaki59@users.noreply.github.com>
2024-06-05 15:17:51 +0900

    topページ 3

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,11 +1,11 @@
-<header class="top-0 left-0 w-[calc(100%-2rem)] h-[80px] flex justify-center items-center bg-white mx-4 my-2 rounded-3xl">
+<header class="relative top-0 left-0 w-[calc(100%-2rem)] h-[80px] flex justify-center items-center bg-white mx-4 my-2 rounded-3xl">
     <div class="text-nowrap absolute left-0">
-        <i class="bi bi-chevron-left px-4"></i>販売ページに戻る
+        <i class="bi bi-chevron-left px-4"></i><span class="hidden md:inline-block">販売ページに戻る</span>
     </div>
     <div class="flex h-[60px] justify-center items-center">
         <div class="h-full flex justify-center items-center">
-            <a href="{{ route('index') }}" class="h-full flex items-center">
-                <img src="{{ asset('/img/header-logo.jpg') }}" alt="Rio flower" class="h-full w-auto object-cover">
+            <a href="{{ route('index') }}" class="h-full flex items-center justify-center">
+                <img src="{{ asset('/img/header-logo.jpg') }}" alt="Rio flower" class="w-[70%] h-auto md:h-full md:w-auto object-cover">
             </a>
         </div>
     </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,26 @@
 <x-template title="Rio flower" css="app.css">
-    <div class="flex mx-4 my-4">
-        <div class="w-[50%] relative">
-            <img src="{{ asset('/img/top1.png') }}" alt="flower" class="w-full rounded-l-3xl object-cover">
-            <div class="absolute left-32 top-80 text-white text-5xl">
+    <div class="flex mx-4 my-4 h-full relative">
+        <div class="relative">
+            <img src="{{ asset('/img/top1.png') }}" alt="flower" class="rounded-l-3xl object-cover h-full">
+            <div class="md:hidden absolute left-1/2 -translate-x-1/2 top-1/4 text-xl text-nowrap">
+                <p class="text-white">Step by Step Bloom by Bloom</p>
+                <p class="text-[#DA9F9F] bg-white p-2">Find Your Perfect Dry Flower</p>
+            </div>
+            <div class="absolute left-1/2 -translate-x-1/2 top-1/2 text-2xl md:left-32 md:-translate-x-0 md:top-64 md:text-5xl text-nowrap text-white font-bold">
                 簡単な質問に答えて<br>
                 あなたにピッタリな花を<br>
                 見つけよう
             </div>
         </div>
-        <div class="w-[50%] relative">
-            <img src="{{ asset('/img/top2.png') }}" alt="flower" class="w-full rounded-r-3xl object-cover">
-            <div class="absolute left-10 top-80 text-xl">
+        <div class="relative hidden md:inline-block">
+            <img src="{{ asset('/img/top2.png') }}" alt="flower" class="rounded-r-3xl object-cover h-full">
+            <div class="absolute left-10 top-64 text-xl">
                 <p class="text-white">Step by Step Bloom by Bloom</p>
                 <p class="text-[#DA9F9F] bg-white p-2">Find Your Perfect Dry Flower</p>
             </div>
+        </div>
+        <div class="pink-btn block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center absolute bottom-16 left-1/2 -translate-x-1/2">
+            <a href="{{ route('ShowQuestionPage') }}">花診断を始める</a>
         </div>
     </div>
 </x-template>
