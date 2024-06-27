74b054c228411be14b81b3510a2878641de11d04
nozaki59 <107387074+nozaki59@users.noreply.github.com>
2024-06-27 15:04:05 +0900

    question.js

--- resources/js/question.js
+++ resources/js/question.js
@@ -506,60 +506,6 @@ function CalPriority(scoreArray) {
     SortScore(scoreArray);
 }
 
-// 顧客情報追加
-SEND_BTN.addEventListener("click",function(){
-    const NAME = document.getElementById("customer-name").value;
-    const ADDRESS = document.getElementById("customer-address").value;
-    const EMAIL = document.getElementById("customer-mail").value;
-    const ID = SEND_BTN.getAttribute("data-id");
-    sessionStorage.setItem('test', JSON.stringify(scoreArray))
-    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-    fetch('/question', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-TOKEN': csrfToken
-        },
-        body: JSON.stringify({
-            name: NAME,
-            address: ADDRESS,
-            email: EMAIL,
-            product_id: ID,
-        })
-    })
-        .then(response => {
-            if (!response.ok) {
-                if (response.status === 422) {
-                    return response.json().then(data => {
-                        throw { validationErrors: data.errors };
-                    });
-                }
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data.redirect) {
-                window.alert('顧客情報を追加しました');
-            } else if (data.message) {
-                alert(data.message);
-            }
-        })
-        .catch(error => {
-            if (error.validationErrors) {
-                let errorMessage = '';
-                for (const field in error.validationErrors) {
-                    errorMessage += `${error.validationErrors[field].join('\n')}\n`;
-                }
-                window.alert(errorMessage);
-            } else {
-                console.error('Error:', error);
-                window.alert('顧客情報の追加に失敗しました');
-            }
-        });
-});
-
 // 結果画面再表示
 function ReShowResult(scoreData) {
     let scoreArray = scoreData;
