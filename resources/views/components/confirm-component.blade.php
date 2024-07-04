e1050824231a939cf994dd1047906b4d7344cd9d
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-07-04 14:27:46 +0900

    update:ユーザー/確認画面：背景に装飾がなかったため、装飾追加（案）

--- resources/views/components/confirm-component.blade.php
+++ resources/views/components/confirm-component.blade.php
@@ -1,5 +1,20 @@
-<div id="confirm_container" class="confirm-container hide gap-6 pt-10">
-    <h1 class="font-bold text-base md:text-2xl text-center mb-4 text-main-brown">以下の回答内容でよろしいですか？</h1>
-    <div id="confirm_answers_container" class="flex flex-col items-center gap-6 bg-main-brown w-[90%] max-h-[50dvh] py-8 rounded-2xl overflow-y-auto"></div>
+<div id="confirm_container" class="confirm-container hide w-full flex flex-col items-center justify-center gap-4">
+    <div id="confirm_answers_container" class="flex flex-col items-center gap-6 bg-main-brown w-[50%] max-h-[60dvh] py-8 rounded-2xl overflow-y-auto"></div>
     <button id="show_result_btn" class="pink-btn">診断結果を見る</button>
 </div>
+<style>
+    #confirm_container {
+        background-image: url('{{asset("storage/img/confirm.jpg")}}'); /* ここに画像のパスを入力 */
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        position: relative;
+        height: 100%;
+        width: 100%;
+        z-index: 1;
+    }
+    #confirm_answers_container {
+        background-color: rgba(191, 158, 116, 0.8); /* 半透明の背景色 */
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 8px 24px rgba(0, 0, 0, 0.2); /* 浮き出るような影 */
+    }
+</style>
