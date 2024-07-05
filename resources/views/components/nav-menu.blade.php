a55f24f2848a3b21cb1d824799a6ac14eec6e46f
nozaki59 <107387074+nozaki59@users.noreply.github.com>
2024-07-05 17:19:10 +0900

    fix: 結果画面 フェード処理part2

--- resources/views/components/nav-menu.blade.php
+++ resources/views/components/nav-menu.blade.php
@@ -1,7 +1,7 @@
 <div id="nav-menu" class="flex flex-col gap-2 bg-main-bg fixed left-0 top-0 w-80 h-full p-0 border-r justify-start duration-500 z-50 non-active rounded-r-xl">
 {{--    "top-0"を消して"h-full"→"h-4/5"にするとヘッダー・フッターに被らないようになる--}}
     <div class="w-full h-full m-0 p-2 flex gap-2 justify-center items-center relative">
-        <button id="navToggleButton" class="absolute top-1/3 md:-right-8 md:w-8 md:h-28 -right-6 w-6 h-24 rounded-r-xl bg-top-button-pink text-white border-t border-r border-b flex items-center justify-center">
+        <button id="navToggleButton" class="absolute top-16 md:top-1/3 md:-right-8 md:w-8 md:h-28 -right-6 w-6 h-24 rounded-r-xl bg-top-button-pink text-white border-t border-r border-b flex items-center justify-center">
             <div id="toggleHide" class="hidden">
                 <i class="bi bi-chevron-double-left"></i>
                 <span class="nav-text">
