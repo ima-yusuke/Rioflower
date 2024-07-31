4632fe0421c2b7e83e8b65c41fd478255ff05db3
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-07-31 16:20:51 +0900

    fix:管理画面/商品登録：priorityのデータ型がintだったため1.2など小数が入らない。そのためdecimal型に変更用のマイグレーションファイル作成。

---
+++ database/migrations/2024_07_31_135724_modify_priority_in_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            // priority カラムを decimal 型に変更
+            $table->decimal('priority', 3, 1)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            // 逆の操作を定義
+            $table->integer('priority')->change();
+        });
+    }
+};
