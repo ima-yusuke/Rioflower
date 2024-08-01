f7d4cdb6094f776ab914d58802c02bc7c4d482a3
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-01 16:32:09 +0900

    add:コメントアウト削除

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -20,21 +20,6 @@
 <main class="flex-1">
     {{ $slot }}
 </main>
-<button id="my-button" class="absolute bottom-20 z-50">Click me</button>
 <x-footer></x-footer>
 </body>
-<!-- Google Tag Manager (noscript) -->
-{{--<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJ8JH69L"--}}
-{{--                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>--}}
-<!-- End Google Tag Manager (noscript) -->
-<script>
-    document.getElementById('my-button').addEventListener('click', function() {
-        gtag('event', 'button_click', {
-            'event_category': 'engagement',
-            'event_label': 'homepage_button',
-            'value': 1
-        });
-    });
-</script>
-
 </html>
