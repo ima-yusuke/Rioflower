356c73dbfeb724a6d5da7e10c8b0fd1d27812552
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-01 16:37:51 +0900

    add:rioflower用にanalyticsアカウント変更

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -9,13 +9,13 @@
 <body class="bg-main-bg h-full flex flex-col">
 <x-header></x-header>
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-332NXEEPRL"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1SS3MSY633"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-332NXEEPRL');
+    gtag('config', 'G-1SS3MSY633');
 </script>
 <main class="flex-1">
     {{ $slot }}
