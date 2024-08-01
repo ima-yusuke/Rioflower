0fe4a96d022adcf85fe28836120353b4bf33cae6
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-01 12:08:52 +0900

    add:Google Analyticsテスト用実装

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -8,10 +8,29 @@
 </x-head>
 <body class="bg-main-bg h-full flex flex-col">
 <x-header></x-header>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-332NXEEPRL"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-332NXEEPRL');
+</script>
 <main class="flex-1">
     {{ $slot }}
 </main>
+<button id="my-button" class="absolute bottom-20 z-50">Click me</button>
 <x-footer></x-footer>
 </body>
+<script>
+    document.getElementById('my-button').addEventListener('click', function() {
+        gtag('event', 'button_click', {
+            'event_category': 'engagement',
+            'event_label': 'homepage_button',
+            'value': 1
+        });
+    });
+</script>
 
 </html>
