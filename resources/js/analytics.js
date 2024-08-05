8349dec868ff56c294755ddd53c1eda7ec87efee
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-05 14:39:14 +0900

    add:analytics.js：ニックネームやトップから質問画面への遷移も記録されたため質問画面になってから検証開始12

--- resources/js/analytics.js
+++ resources/js/analytics.js
@@ -1,43 +1,20 @@
 let preventUnloadTracking = false;
-//
-// if (sessionStorage.getItem('analyticsFlag') === 'true') {
-//     preventUnloadTracking = true;
-//     sessionStorage.removeItem('analyticsFlag');
-// }
-//
-//
-// // 現在のURLをチェックして、特定のURLでのみbeforeunloadイベントを設定
-// if (window.location.href.startsWith("https://test.flaver-rio.com/question")) {
-//
-//     window.addEventListener('beforeunload', function(event) {
-//         // ボタンのクリックで遷移する場合は記録をスキップ
-//         if (preventUnloadTracking) {
-//             return;
-//         }
-//
-//         // ページ離脱イベントを記録
-//         gtag('event', "カウント", {
-//             'event_category': 'ページ離脱',
-//             'event_label': 'ページ離脱',
-//             'value': 1
-//         });
-//     });
-//
-//     // ボタンクリックイベントを監視（question→checkに遷移時）
-//     const SEND_BTN = document.getElementById('send-btn');
-//     if (SEND_BTN!=null){
-//         SEND_BTN.addEventListener('click', function(event) {
-//             // ボタンクリックによる遷移では記録をスキップ
-//             preventUnloadTracking = true;
-//         });
-//     }
-// }
 
 // ボタンクリックイベントを監視（question→checkに遷移時）
 const SEND_BTN = document.getElementById('send-btn');
 
 if (SEND_BTN!=null){
     SEND_BTN.addEventListener('click', function(event) {
+        // ボタンクリックによる遷移では記録をスキップ
+        preventUnloadTracking = true;
+    });
+}
+
+// ボタンクリックイベントを監視（question→topに遷移時）
+const BACK_START_BTN = document.getElementById('back-start-btn');
+
+if (BACK_START_BTN!=null){
+    BACK_START_BTN.addEventListener('click', function(event) {
         // ボタンクリックによる遷移では記録をスキップ
         preventUnloadTracking = true;
     });
@@ -50,7 +27,7 @@ window.addEventListener('beforeunload', function(event) {
     }
 
     // ページ離脱イベントを記録
-    gtag('event', "カウントテスト用", {
+    gtag('event', "テスト", {
         'event_category': 'ページ離脱',
         'event_label': 'ページ離脱',
         'value': 1
