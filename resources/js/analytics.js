692351c0ea58a9c13adbd00fa4166d20ee99fde6
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-05 11:58:50 +0900

    add:analytics.js：ニックネームやトップから質問画面への遷移も記録されたため質問画面になってから検証開始8

--- resources/js/analytics.js
+++ resources/js/analytics.js
@@ -9,6 +9,8 @@ if (SHOW_QUESTION_BTN!=null){
     });
 }
 
+console.log(preventUnloadTracking);
+
 // 現在のURLをチェックして、特定のURLでのみbeforeunloadイベントを設定
 if (window.location.href.startsWith("https://test.flaver-rio.com/question")) {
 
@@ -17,7 +19,6 @@ if (window.location.href.startsWith("https://test.flaver-rio.com/question")) {
         if (preventUnloadTracking) {
             return;
         }
-
 
         // ページ離脱イベントを記録
         gtag('event', "カウント", {
@@ -29,16 +30,21 @@ if (window.location.href.startsWith("https://test.flaver-rio.com/question")) {
 
     // ボタンクリックイベントを監視（question→checkに遷移時）
     const SEND_BTN = document.getElementById('send-btn');
-    SEND_BTN.addEventListener('click', function(event) {
-        // ボタンクリックによる遷移では記録をスキップ
-        preventUnloadTracking = true;
-    });
+    if (SEND_BTN!=null){
+        SEND_BTN.addEventListener('click', function(event) {
+            // ボタンクリックによる遷移では記録をスキップ
+            preventUnloadTracking = true;
+        });
+    }
+
 
     // ボタンクリックイベントを監視（question→topに遷移時）
     const BACK_START_BTN = document.getElementById(' back-start-btn');
-    BACK_START_BTN.addEventListener('click', function(event) {
-        // ボタンクリックによる遷移では記録をスキップ
-        preventUnloadTracking = true;
-    });
+    if(BACK_START_BTN!=null) {
+        BACK_START_BTN.addEventListener('click', function (event) {
+            // ボタンクリックによる遷移では記録をスキップ
+            preventUnloadTracking = true;
+        });
+    }
 
 }
