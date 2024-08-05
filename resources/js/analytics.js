44cdf1e21499b8233f2c21ac9b2442c5237235b4
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-05 14:08:49 +0900

    add:analytics.js：ニックネームやトップから質問画面への遷移も記録されたため質問画面になってから検証開始12

--- resources/js/analytics.js
+++ resources/js/analytics.js
@@ -1,29 +1,22 @@
+let preventUnloadTracking = false;
 
-// グローバルスコープで変数を定義
-let preventUnloadTracking =false;
-
-// セッションストレージからフラグを取得
-if(sessionStorage.getItem('nickNameFlag')===true){
+if (sessionStorage.getItem('flag') === 'true') {
     preventUnloadTracking = true;
-    sessionStorage.removeItem('nickNameFlag');
+    sessionStorage.removeItem('flag');
 }
 
-if(sessionStorage.getItem('topFlag')===true) {
-    preventUnloadTracking = true;
-    sessionStorage.removeItem('topFlag');
-}
 
-// questionページでのbeforeunloadイベントの設定
+// 現在のURLをチェックして、特定のURLでのみbeforeunloadイベントを設定
 if (window.location.href.startsWith("https://test.flaver-rio.com/question")) {
-    window.addEventListener('beforeunload', function(event) {
 
-        // preventUnloadTrackingがtrueなら記録をスキップ
+    window.addEventListener('beforeunload', function(event) {
+        // ボタンのクリックで遷移する場合は記録をスキップ
         if (preventUnloadTracking) {
             return;
         }
 
         // ページ離脱イベントを記録
-        gtag('event', "カウント離脱数", {
+        gtag('event', "カウント", {
             'event_category': 'ページ離脱',
             'event_label': 'ページ離脱',
             'value': 1
@@ -32,11 +25,21 @@ if (window.location.href.startsWith("https://test.flaver-rio.com/question")) {
 
     // ボタンクリックイベントを監視（question→checkに遷移時）
     const SEND_BTN = document.getElementById('send-btn');
-    if (SEND_BTN != null) {
+    if (SEND_BTN!=null){
         SEND_BTN.addEventListener('click', function(event) {
-            sessionStorage.setItem('preventUnloadTracking', 'true'); // セッションストレージにフラグを設定
+            // ボタンクリックによる遷移では記録をスキップ
+            preventUnloadTracking = true;
         });
     }
-}
 
-preventUnloadTracking = false; // フラグをリセット
+
+    // ボタンクリックイベントを監視（question→topに遷移時）
+    const BACK_START_BTN = document.getElementById(' back-start-btn');
+    if(BACK_START_BTN!=null) {
+        BACK_START_BTN.addEventListener('click', function (event) {
+            // ボタンクリックによる遷移では記録をスキップ
+            preventUnloadTracking = true;
+        });
+    }
+
+}
