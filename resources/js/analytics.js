25cf67a7aad12a7b719af90974984d517c635aeb
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-05 10:50:03 +0900

    add:analytics.js：ニックネームやトップから質問画面への遷移も記録されたため質問画面になってから検証開始

--- resources/js/analytics.js
+++ resources/js/analytics.js
@@ -1,8 +1,11 @@
 // ページがアンロードされる（離れる）直前にイベントを送信
 window.addEventListener('beforeunload', function (cu){
-    gtag('event', "ページ離脱", {
-        'event_category': 'ページ離脱',
-        'event_label': 'ページ離脱',
-        'value': 1
-    });
+    // 現在のURLが指定された範囲外ならば記録
+    if (window.location.href.startsWith("https://test.flaver-rio.com/question")) {
+        gtag('event', "ページ離脱数", {
+            'event_category': 'ページ離脱',
+            'event_label': 'ページ離脱',
+            'value': 1
+        });
+    }
 });
