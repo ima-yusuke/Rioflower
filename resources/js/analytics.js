25fdef3e1e0f03d98534bde7063638c9bbfd5ae8
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-05 11:08:14 +0900

    add:analytics.js：ニックネームやトップから質問画面への遷移も記録されたため質問画面になってから検証開始3

--- resources/js/analytics.js
+++ resources/js/analytics.js
@@ -9,7 +9,7 @@ if (window.location.href.startsWith("https://test.flaver-rio.com/question")) {
         }
 
         // ページ離脱イベントを記録
-        gtag('event', "ページ離脱数", {
+        gtag('event', "離脱数", {
             'event_category': 'ページ離脱',
             'event_label': 'ページ離脱',
             'value': 1
@@ -17,9 +17,17 @@ if (window.location.href.startsWith("https://test.flaver-rio.com/question")) {
     });
 
     // ボタンクリックイベントを監視（question→checkに遷移時）
-    const button = document.getElementById('send-btn');
-    button.addEventListener('click', function(event) {
+    const SEND_BTN = document.getElementById('send-btn');
+    SEND_BTN.addEventListener('click', function(event) {
         // ボタンクリックによる遷移では記録をスキップ
         preventUnloadTracking = true;
     });
+
+    // ボタンクリックイベントを監視（question→topに遷移時）
+    const BACK_START_BTN = document.getElementById(' back-start-btn');
+    BACK_START_BTN.addEventListener('click', function(event) {
+        // ボタンクリックによる遷移では記録をスキップ
+        preventUnloadTracking = true;
+    });
+
 }
