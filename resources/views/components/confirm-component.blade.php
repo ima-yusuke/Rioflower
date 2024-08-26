3de50d1103097ae66aa405585eadf3fc4191ed69
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-26 13:01:17 +0900

    fix:顧客より回答を最低でも4つ表示して欲しいと依頼があったため、質問・確認のスマホ画面コンテナheightを300pxから330pxにしfont-sizeも12pxに修正。

--- resources/views/components/confirm-component.blade.php
+++ resources/views/components/confirm-component.blade.php
@@ -1,22 +1,18 @@
 <div id="confirm_container" class="confirm-container hide w-full h-full flex flex-col items-center justify-center gap-4">
     {{--タイトル--}}
     <div class="absolute top-6 md:top-0 md:static" id="confirm_title">
-        <p class="text-center text-2xl text-main-brown" id="test">Check</p>
-        <div class="flex justify-center text-center">
-            <p class="border-t border-main-brown w-[50px] mb-2 md:mb-4"></p>
-        </div>
         <p class="text-main-brown text-sm md:text-xl">以下の回答内容でよろしいですか</p>
     </div>
 
     {{--確認コンテナ--}}
-    <div class="flex items-center rounded-3xl relative" id="confirm">
-        <div id="confirm_answers_container" class="relative flex flex-col justify-center items-center gap-6 py-4 md:py-8 w-[300px] h-[300px] md:w-[500px] md:h-[450px] bg-main-brown rounded-3xl shadow-custom-dark">
+    <div class="flex items-center rounded-3xl relative h-full w-full bg-blue-400" id="confirm">
+        <div id="confirm_answers_container" class="relative flex flex-col justify-center items-center gap-6 py-4 md:py-8 w-[300px] h-[330px] md:w-[500px] md:h-[450px] bg-main-brown rounded-3xl shadow-custom-dark">
             <div class="flex flex-col items-center overflow-y-auto py-4 md:py-0 gap-6 mx-8 md:mx-8 h-full w-full" id="confirm_box"></div>
         </div>
     </div>
 
     {{--結果画面へ遷移ボタン--}}
-    <button id="show_result_btn" class="absolute bottom-12 md:absolute md:bottom-4 md:right-8 pink-btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">診断結果を見る→</button>
+    <button id="show_result_btn" class="absolute md:absolute bottom-6 md:bottom-4 md:right-8 pink-btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">診断結果を見る→</button>
 </div>
 <style>
     #confirm {
@@ -31,7 +27,7 @@
     }
     @media (max-width: 767px) {
         #confirm {
-            height: 300px;
+            height: 330px;
             width: 300px;
         }
     }
--- resources/views/components/question-component.blade.php
+++ resources/views/components/question-component.blade.php
@@ -1,21 +1,21 @@
-<div id="question_container" class="question-container hide w-full pb-4 md:pb-0">
+<div id="question_container" class="question-container hide w-full pb-4 md:pb-0 h-full relative">
 
-    <div class="flex flex-col justify-center items-center gap-6 w-full">
+    <div class="flex flex-col justify-center items-center gap-2 w-full h-full">
         {{--残質問数--}}
-        <div id="question_num" class="text-main-brown text-sm md:text-base"></div>
+        <div id="question_num" class="text-main-brown text-sm md:text-base absolute top-2 left-1/2 transform -translate-x-1/2"></div>
 
         {{--画像と質問--}}
-        <div class="flex flex-col items-center justify-center md:flex-row gap-6 md:gap-28 w-full">
+        <div class="flex flex-col items-center justify-center md:flex-row gap-4 md:gap-28 w-full h-full">
             {{--左側コンテンツ（画像）--}}
-            <div id="question_img" class="w-[120px] h-[120px] md:w-[500px] md:h-[500px] rounded-full md:rounded-3xl">
+            <div id="question_img" class="w-[120px] h-[120px] md:w-[500px] md:h-[500px] rounded-full md:rounded-3xl z-10">
                 <img src="{{ asset('storage/img/botanical.jpg') }}" class="question-img back" draggable="false">
             </div>
 
             {{--右側コンテンツ（質問＆選択肢）--}}
-            <div class="flex items-center rounded-3xl" id="question">
-                <div id="bg" class="relative flex flex-col justify-center gap-6 md:gap-12 md:py-8 w-[300px] h-[300px] md:w-[500px] md:h-[500px] bg-main-brown rounded-3xl shadow-custom-dark">
+            <div class="flex items-center rounded-3xl -mt-10 md:mt-0" id="question">
+                <div id="bg" class="relative flex flex-col justify-center gap-6 md:gap-12  w-[300px] h-[330px] md:w-[500px] md:h-[500px] bg-main-brown rounded-3xl shadow-custom-dark">
                     {{--質問文--}}
-                    <div id="question_text" class="font-bold text-top-white md:text-xl text-center mx-4 md:mx-8"></div>
+                    <div id="question_text" class="font-bold text-top-white md:text-xl text-center mx-4 md:mx-8 mt-6"></div>
                     {{--選択肢--}}
                     <div id="question_answers_container" class="flex flex-col items-center overflow-y-auto gap-4 md:gap-6 mx-4 md:mx-8 h-full"></div>
                 </div>
@@ -24,10 +24,9 @@
     </div>
 
     {{--戻るボタン--}}
-    <button id="back_btn" class="absolute md:bottom-16 md:left-4 text-main-brown bg-white rounded-full shadow-back-btn-shadow flex justify-center items-center h-[60px] w-[60px] ml-4 md:mt-4 font-bold hide">
-       <span class="text-sm">戻る</span>
+    <button id="back_btn" class="absolute z-20 bottom-2 md:bottom-16 md:left-4 text-main-brown bg-white rounded-full shadow-back-btn-shadow flex justify-center items-center h-[55px] w-[55px] md:h-[60px] md:w-[60px] ml-4 mt-2 md:mt-4 font-bold hide">
+       <span class="text-[10px] md:text-sm">戻る</span>
     </button>
-
 </div>
 <style>
     #question {
@@ -42,7 +41,7 @@
     }
     @media (max-width: 767px) {
         #question {
-            height: 300px;
+            height: 330px;
             width: 300px;
         }
     }
