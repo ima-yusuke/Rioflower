4004efd649091a44c2ac5a1f6f4587453217b312
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-10-03 12:15:41 +0900

    fix:footerに補助金対象の文言を追加

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,8 @@
-<footer class="w-full min-h-[25px] md:min-h-[30px] flex justify-center bg-gray-800 z-40">
+<footer class="w-full min-h-[25px] md:min-h-[30px] flex justify-center bg-gray-800 z-40 relative">
     <div class="flex justify-center items-center">
         <p class="text-white">
             &copy; 2024 @if(date("Y")!=2024)-{{ date("Y") }} @endif flaver All Reserved.
         </p>
     </div>
+    <p class="text-white text-[8px] md:text-xs absolute right-2 top-1/2 transform -translate-y-1/2">第9回補助金対象</p>
 </footer>
